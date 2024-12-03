bf45e9368e34eb2cd13af368469d47003b2c2743
Muhammad Shafay <shafin22405@gmail.com>
2024-12-03 19:22:30 +0500

    completed till appwrite and create account

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,7 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>Sign-in</div>
-  )
-}
+const page = () => <AuthForm type="sign-in"/>
+
 
 export default page
