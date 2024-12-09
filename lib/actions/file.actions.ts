961490dec191db6b0a4ff55af017e7de02bc7dea
Muhammad Shafay <shafin22405@gmail.com>
2024-12-09 18:29:47 +0500

    completed till sorting

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -61,24 +61,44 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number
+) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
       Query.contains("users", [currentUser.email]),
     ]),
   ];
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+  
+    queries.push(orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy));   
+  }
 
   return queries;
 };
-export const getFiles = async () => {
+export const getFiles = async ({
+  types = [],
+  searchText = "",
+  sort = "$createdAt-desc",
+  limit,
+}: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
     const currentUser = await getCurrentUser();
     if (!currentUser) throw new Error("User not found");
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
@@ -101,7 +121,6 @@ export const renameFile = async ({
   const { databases } = await createAdminClient();
 
   try {
-
     const newName = `${name}.${extension}`;
     const updatedFile = await databases.updateDocument(
       appwriteConfig.databaseId,
@@ -113,7 +132,6 @@ export const renameFile = async ({
     revalidatePath(path);
 
     return parseStringify(updatedFile);
-
   } catch (error) {
     handleError(error, "Failed to rename file");
   }
@@ -137,7 +155,6 @@ export const updateFileUsers = async ({
     revalidatePath(path);
 
     return parseStringify(updatedFile);
-
   } catch (error) {
     handleError(error, "Failed to share file");
   }
@@ -154,7 +171,7 @@ export const deleteFile = async ({
     const deletedFile = await databases.deleteDocument(
       appwriteConfig.databaseId,
       appwriteConfig.filesCollectionId,
-      fileId,
+      fileId
     );
 
     if (deletedFile) {
@@ -164,7 +181,6 @@ export const deleteFile = async ({
     revalidatePath(path);
 
     return parseStringify({ value: "success" });
-
   } catch (error) {
     handleError(error, "Failed to share file");
   }
